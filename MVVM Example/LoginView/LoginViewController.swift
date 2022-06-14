0e991c560ebca2c473512829c8d124d591301c77
Jose Arsuaga <jose.arsuaga@walmart.com>
2022-06-13 19:30:46 -0500

    Create network service

--- MVVM Example/LoginView/LoginViewController.swift
+++ MVVM Example/LoginView/LoginViewController.swift
@@ -11,10 +11,12 @@ class LoginViewController: UIViewController {
     
     @IBOutlet private weak var emailTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
+    
+    private let viewModel = LoginViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBAction func loginButtonTapped() {
---
+++ MVVM Example/NetworkService.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkService.swift
+//  MVVM Example
+//
+//  Created by Jose Arsuaga on 13/06/22.
+//
+
+import Foundation
+
+final class NetworkService {
+    static let shared = NetworkService()
+    private var user: User?
+    
+    private init() { }
+    
+    func login(email: String, password: String, completion: @escaping (Bool) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            if email == "test@test.com" && password == "password" {
+                self?.user = User(firstName: "José",
+                            lastName: "Arsuaga",
+                            email: email,
+                            age: 32)
+                completion(true)
+            } else {
+                self?.user = nil
+                completion(false)
+            }
+        }
+    }
+}
